ed12d8a8142cb71159e8ebf89402f4479c4c3d3b
zenon-paul <sakura1024skr@gmail.com>
2025-11-29 15:33:47 +0900

    update

--- basic_note.v
+++ basic_note.v
@@ -1,4 +1,63 @@
-(*nandb*)
+(*Enumerated Types*)
+Inductive day : Type :=
+  | monday : day
+  | tuesday : day
+  | wednesday : day
+  | thursday : day
+  | friday : day
+  | saturday : day
+  | sunday : day.
+
+Definition next_weekday (d:day) : day :=
+  match d with
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
+
+Eval simpl in (next_weekday friday).
+Eval simpl in (next_weekday (next_weekday saturday)).
+
+Example test_next_weekday:
+  (next_weekday (next_weekday saturday)) = tuesday.
+Proof. simpl. reflexivity. Qed.
+
+Inductive bool : Type :=
+  | true : bool
+  | false : bool.
+
+Definition negb (b:bool) : bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+
+Definition andb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => b2
+  | false => false
+  end.
+
+Definition orb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => true
+  | false => b2
+  end.
+
+Example test_orb1: (orb true false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_orb2: (orb false false) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_orb3: (orb false true ) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_orb4: (orb true true ) = true.
+Proof. simpl. reflexivity. Qed.
+
+(*plactice nandb*)
 Definition nandb (b1:bool) (b2:bool) : bool :=
   match b1 with
   | false => true
@@ -13,8 +72,9 @@ Example test_nandb3: (nandb false true) = true.
 Proof. simpl. reflexivity. Qed.
 Example test_nandb4: (nandb true true) = false.
 Proof. simpl. reflexivity. Qed.
+(*end plactice nandb*)
 
-(*andb3*)
+(*plactice andb3*)
 Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
   match b1 with
   | true => andb b2 b3
@@ -29,15 +89,109 @@ Example test_andb33: (andb3 true false true) = false.
 Proof. simpl. reflexivity. Qed.
 Example test_andb34: (andb3 true true false) = false.
 Proof. simpl. reflexivity. Qed.
+(*end plactice andb3*)
 
-(*factorial*)
+Check (negb true).
+Check negb.
+
+
+Module Playground1.
+
+Inductive nat : Type :=
+  | O : nat
+  | S : nat → nat.
+
+Definition pred (n : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => n'
+  end.
+
+End Playground1.
+
+
+Definition minustwo (n : nat) : nat :=
+  match n with
+    | O => O
+    | S O => O
+    | S (S n') => n'
+  end.
+
+Check (S (S (S (S O)))).
+Eval simpl in (minustwo 4).
+
+Check S.
+Check pred.
+Check minustwo.
+
+Fixpoint evenb (n:nat) : bool :=
+  match n with
+  | O => true
+  | S O => false
+  | S (S n') => evenb n'
+  end.
+
+Definition oddb (n:nat) : bool := negb (evenb n).
+
+Example test_oddb1: (oddb (S O)) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_oddb2: (oddb (S (S (S (S O))))) = false.
+Proof. simpl. reflexivity. Qed.
+
+
+Module Playground2.
+
+Fixpoint plus (n : nat) (m : nat) : nat :=
+  match n with
+    | O => m
+    | S n' => S (plus n' m)
+  end.
+
+Eval simpl in (plus (S (S (S O))) (S (S O))).
+
+Fixpoint mult (n m : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => plus m (mult n' m)
+  end.
+
+Fixpoint minus (n m:nat) : nat :=
+  match n, m with
+  | O , _ => O
+  | S _ , O => n
+  | S n', S m' => minus n' m'
+  end.
+
+End Playground2.
+
+Fixpoint exp (base power : nat) : nat :=
+  match power with
+    | O => S O
+    | S p => mult base (exp base p)
+  end.
+
+Example test_mult1: (mult 3 3) = 9.
+Proof. simpl. reflexivity. Qed.
+
+(*plactice factorial*)
 Fixpoint factrial (n : nat) : nat :=
   match n with
   | 0 => 1
   | S n' => mult n (factrial n')
   end.
 
-(*blt_nat*)
+Example test_factorial1: (factorial 3) = 6.
+Proof. simpl. reflexivity. Qed.
+Example test_factorial2: (factorial 5) = (mult 10 12).
+Proof. simpl. reflexivity. Qed.
+(*end plactice factorial*)
+
+Notation "x + y" := (plus x y) (at level 50, left associativity) : nat_scope.
+Notation "x - y" := (minus x y) (at level 50, left associativity) : nat_scope.
+Notation "x * y" := (mult x y) (at level 40, left associativity) : nat_scope.
+
+Check ((0 + 1) + 1).
+
 Fixpoint beq_nat (n m : nat) : bool :=
   match n with
   | O => match m with
@@ -60,10 +214,37 @@ Fixpoint ble_nat (n m : nat) : bool :=
       end
   end.
 
+Example test_ble_nat1: (ble_nat 2 2) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_ble_nat2: (ble_nat 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_ble_nat3: (ble_nat 4 2) = false.
+Proof. simpl. reflexivity. Qed.
+
+(*plactice blt_nat*)
 Definition blt_nat (n m : nat) : bool := 
   xorb (beq_nat n m) (ble_nat n m).
 
-(*simple_plus*)
+Example test_blt_nat1: (blt_nat 2 2) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat2: (blt_nat 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat3: (blt_nat 4 2) = false.
+Proof. simpl. reflexivity. Qed.
+(*end plactice blt_nat*)
+
+Theorem plus_O_n : forall n:nat, 
+  0 + n = n.
+Proof.
+  simpl. reflexivity. Qed.
+
+Theorem plus_O_n' : forall n:nat, 
+  0 + n = n.
+Proof.
+  reflexivity. Qed.
+
+(*plactice simple_plus*)
 Eval simpl in (forall n:nat, n + 0 = n).
 
 Eval simpl in (forall n:nat, 0 + n = n).
+(*end plactice*)
